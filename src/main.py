94ffb21326e8286711db6d852e877a46c9e063a6
Tina245445 <s245445@dtu.dk>
2026-09-10 12:10:40 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,8 @@
+motors = int(input("How many motors are carrying the packages? "))
+package_weight = int(input("How many kg of packages do we expect? "))
+
+if package_weight / motors <= 12:
+    print("Yes! The conveyor belt can carry the packages.")
+else:
+    print("No. The conveyor belt cannot carry the packages.")
 print("hello!")
